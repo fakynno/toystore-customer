27c4192a59e566bd9438ce6c3c9f1398c77fa785
fakynno <fakynno@gmail.com>
2025-04-16 23:27:47 -0300

    importe inicial do projeto

---
+++ src/main/java/com/toystore/customer/ToystoreCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.toystore.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToystoreCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToystoreCustomerApplication.class, args);
+	}
+
+}
